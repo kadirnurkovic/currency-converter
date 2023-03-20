1e1905d04237a8f839c3a6fa2f0362bd75ac50a3
kadirnurkovic <kadirnurkovic0404@gmail.com>
2023-03-20 15:51:26 +0100

    msg

--- src/assets/MainPage/MainPage.tsx
+++ src/assets/MainPage/MainPage.tsx
@@ -5,14 +5,20 @@ import "./MainPage.css"
 const MainPage = () => {
     
   interface Obj {
-    keys: string[];
-    values: number[];
+    keys: any[];
+    values: any[];
   }
 
   const [currencyData, setCurrencyData] = useState<Obj>({
     keys: [],
     values: [],
   });
+  const [check, setCheck] = useState(1)
+  const [currency, setCurrency] = useState('')
+  const [currencyValue, setCurrencyValue] = useState(1);
+  const [initialCurrencyValue, setInitialCurrencyValue] = useState(1);
+  const [resultOfConvert, setResultOfConvert] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const getData = () => {
     axios
@@ -22,12 +28,15 @@ const MainPage = () => {
       .then((res) => {
         console.log(res)
         setCurrencyData({
-            keys: Object.keys(res.data.results),
-            values: Object.values(res.data.results),
+            keys: Object.entries(res.data.results),
+            values: Object.entries(res.data.results),
         })
       });
   };
+  let amount: number | undefined;
+  const currencyConvertion = (e: number) => {
 
+  }
   useEffect(() => {
     getData();
   }, []);
@@ -36,25 +45,29 @@ const MainPage = () => {
     <div>
       <div className="list">
         <div>
-          <input/>
-          <select>
+          <input onChange={(e) => {e.preventDefault; setInitialCurrencyValue(isNaN(e.target.value) ? 1 : +e.target.value )}} value={initialCurrencyValue}></input>
+          {console.log(initialCurrencyValue)}
+          <select className="selection">
           {currencyData.keys.map((key, index) => (
-          <option key={index}>
-            {key}
+          <option key={index} value={key[0]}>
+            
+            {key[1]}
           </option>
         ))}
           </select>
         </div>
         <div>
-        <input/>
-        <select>
-        {currencyData.keys.map((values, index) => (
-          <option key={index}>
-            {values}
+        <input value={resultOfConvert * initialCurrencyValue}/>
+        <select className="selection" onChange={(e) => {setResultOfConvert(e.target.value)}}>
+        {currencyData.values.map((values, index) => (
+          <option key={index} value={values[1]}>
+            {values[0]}
+            {console.log(values[0])}
           </option>
         ))}
         </select>
         </div>
+        <button onClick={() => {}}>Convert</button>
       </div>
     </div>
   );
